19fb4b6970279307e7a1c237359c1924edee8623
Jonty <jonty.morris@yahoo.co.nz>
2019-02-24 18:15:50 +1300

    Updated favicon/theme

--- partials/head.php
+++ partials/head.php
@@ -1,3 +1,12 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/master.css">
+
+<!-- favicon/theme -->
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#414141">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#1d1e32">
