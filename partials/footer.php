f4faa3b84a49e9efcb7c6772be8b6138fe7f3ec3
Jonty <jonty.morris@yahoo.co.nz>
2019-06-16 15:05:30 +1200

    Fixed email link

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container content grid one-one">
         <div>
-            Want to talk about a project? Send us an email at <a href="mailto:virtualstack.co.nz">contact@virtualstack.co.nz</a> for a free, no obligations quote today!
+            Want to talk about a project? Send us an email at <a href="mailto:contact@virtualstack.co.nz">contact@virtualstack.co.nz</a> for a free, no obligations quote today!
         </div>
         <div class="right">
             <section>
